0363cafc434b49f201d6f9ed506c708cae518996
Conqueror102 <victorvector608@gmail.com>
2025-02-09 01:04:07 +0100

    update

--- src/static/Header.jsx
+++ src/static/Header.jsx
@@ -21,11 +21,24 @@ const Header = ({homeScroll, aboutScroll,serviceScroll,contactScroll, enroll }) 
           <div className="cursor-pointer w-25 flex items-center max-sm:w-16">
             <img className=" " src="" alt="" />
           </div>
+          {/* nav for desktop */}
+          <nav className=" items-center w-74 gap-10  max-md:hidden  ">
+            <ul className="flex gap-2  justify-between w-full font-semibold cursor-pointer  ">
+              <li className=" hover:text-[#16a571]" onClick={homeScroll}>Home</li>
+              <li className="hover:text-[#16a571]" onClick={aboutScroll}>About</li>
+              <li className="hover:text-[#16a571]" onClick={contactScroll}>
+                Contact
+              </li>
+              <li className="max-sm:w-full max-sm:p-2  max-sm:hover:bg-[#16a571]" onClick={serviceScroll} >
+                Service
+              </li>
+            </ul>
+          </nav>
 
           {/* nav */}
         {state && (
-          <div onClick={handleStateClose} className={`max-sm:w-full  max-sm:h-screen max-sm:bg-[#27272765] max-sm:absolute max-sm:top-12  max-sm:right-0 mobil-nav `}>
-              <nav className=" transition-all duration-700 ease-in-out max-sm:flex max-sm:float-right items-center w-74 gap-10  max-sm:text-center max-sm:top-20 max-sm:w-55   max-sm:h-[220px] max-sm:rounded-b-md max-sm:bg-white max-sm:shadow-lg">
+          <div onClick={handleStateClose} className={`max-sm:w-full  max-sm:h-screen max-sm:bg-[#27272765] max-sm:absolute max-sm:top-12  max-sm:right-0  `}>
+              <nav className="md:hidden max-sm:flex max-sm:float-right items-center w-74 gap-10  max-sm:text-center max-sm:top-20 max-sm:w-55   max-sm:h-[220px] max-sm:rounded-b-md max-sm:bg-white max-sm:shadow-lg">
             <ul className="flex gap-2  justify-between w-full font-semibold cursor-pointer  max-sm:flex-col">
               <li className="max-sm:w-full max-sm:p-2 max-sm:border-b max-sm:border-gray-200  max-sm:hover:bg-[#16a571]" onClick={homeScroll}>Home</li>
               <li className="max-sm:w-full max-sm:p-2 max-sm:border-b max-sm:border-gray-200 max-sm:hover:bg-[#16a571]" onClick={aboutScroll}>About</li>
